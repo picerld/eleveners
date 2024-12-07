9e7d88ff45ad2f073d4e21ae433eff1983ddb234
picerld <raficahyadi1221@gmail.com>
2024-12-07 19:11:18 +0700

    fix: body on features

--- src/sections/features.tsx
+++ src/sections/features.tsx
@@ -1,18 +1,27 @@
-'use client'
+'use client';
 
-import Avatar from '@/components/ui/avatat'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import Marquee from 'react-fast-marquee'
+import Avatar from '@/components/ui/avatat';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Marquee from 'react-fast-marquee';
+
+interface Review {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
 
 export default function Features() {
-  const [review, setReview] = useState([])
+  const [review, setReview] = useState<Review[]>([]);
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
-      setReview(res.data)
-    })
-  }, [])
+    axios
+      .get<Review[]>('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => {
+        setReview(res.data);
+      });
+  }, []);
 
   const features = [
     { sender: 'Rafi Cahyadi' },
@@ -24,10 +33,10 @@ export default function Features() {
   ].map((feature, index) => ({
     ...feature,
     text: review[index]?.body || 'Loading...',
-  }))
+  }));
 
   return (
-    <div id='features'>
+    <div id="features">
       <section className="border-t-border dark:border-t-darkBorder dark:bg-darkBg border-t-2 bg-bg py-20 font-base lg:py-[100px]">
         <h2 className="mb-14 px-5 text-center text-2xl font-heading md:text-3xl lg:mb-20 lg:text-4xl">
           Kata Mereka Lulusan dan Murid <br /> SMKN 11 Bandung
@@ -39,11 +48,9 @@ export default function Features() {
               className="border-border dark:border-darkBorder dark:bg-secondaryBlack shadow-light dark:shadow-dark flex flex-col gap-3 rounded-base border-2 bg-white p-5"
               key={i}
             >
-              <div className='flex gap-3 items-center'>                  
-                <Avatar imageUrl='https://neobrutalism-bento.netlify.app/pfp.png' />
-                <h4 className="text-xl font-heading">
-                  {feature.sender}
-                </h4>
+              <div className="flex gap-3 items-center">
+                <Avatar imageUrl="https://neobrutalism-bento.netlify.app/pfp.png" />
+                <h4 className="text-xl font-heading">{feature.sender}</h4>
               </div>
               <p>{feature.text}</p>
             </div>
@@ -67,5 +74,5 @@ export default function Features() {
         </Marquee>
       </div>
     </div>
-  )
+  );
 }
